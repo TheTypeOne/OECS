7132aa8884ad4dd2fb07f10e801891a9e18b4bdf
TheTypeOne <89516649+TheTypeOne@users.noreply.github.com>
2021-09-17 10:05:02 +0700

    16/09/2021

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -3,10 +3,31 @@
 namespace Lesson1
 {
     class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+{
+	static string globalVariable = "Global variable";
+
+	static void MethodA()
+	{
+		if (globalVariable == "")
+		{
+			string temporalVariable = "Temporal variable";
+			Console.WriteLine(temporalVariable);
+		}
+
+		string localVariable = "Local variable";
+		
+		// Так можно — эта переменная используется в той же области, где и объявлена:
+		Console.WriteLine(localVariable);
+		
+		// Так нельзя — temporalVariable определена только внутри блока if:
+		// Console.WriteLine(temporalVariable); 
+	}
+
+	static void MethodB()
+	{
+		// Console.WriteLine(localVariable); //Нельзя — переменная определена в другом методе.
+		Console.WriteLine(globalVariable); //Можно — это глобальная переменная
+
+	}
+}
 }
